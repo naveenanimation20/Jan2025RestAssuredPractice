0ea8ec686f4f7b2cf8e00b137295a1ee5fc58027
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-10-08 08:19:11 +0530

    Update FakeUserTest.java

--- src/test/java/FakeUserAPI/FakeUserTest.java
+++ src/test/java/FakeUserAPI/FakeUserTest.java
@@ -34,6 +34,54 @@ public class FakeUserTest {
 		System.out.println(userId);
 		
 	}
+
+
+@Test
+	public void createAPetWithAssertionsTest() {
+		
+		RestAssured.baseURI = "https://petstore.swagger.io";
+		
+		//create the object of Pet class:
+		
+		Pet.Category category = new Pet.Category(101, "Dog");
+		
+		List<String> photoUrls =  Arrays.asList("https://dog1.com", "https://dog2.com", "https://dog3.com");
+		
+		Pet.Tag tag1 = new Pet.Tag(201, "Red");
+		Pet.Tag tag2 = new Pet.Tag(202, "Black");
+		Pet.Tag tag3 = new Pet.Tag(202, "White");
+
+		
+		List<Tag> tags = Arrays.asList(tag1, tag2, tag3);
+		
+		Pet pet = new Pet(1, "Tommy", "available", photoUrls, tags, category);
+		
+		Response response = given().log().all()
+			.contentType(ContentType.JSON)
+			.body(pet) //serialization : 
+		.when()
+			.post("/v2/pet");
+		
+		JsonPath js = response.jsonPath();
+		
+		Assert.assertEquals(js.getInt("id"), pet.getId());
+		Assert.assertEquals(js.getInt("category.id"), category.getId());
+		Assert.assertEquals(js.getString("name"), pet.getName());
+		Assert.assertEquals(js.getString("status"), pet.getStatus());
+		Assert.assertEquals(js.getList("photoUrls"), pet.getPhotoUrls());
+//		Assert.assertEquals(js.getInt("tags[0].id"), pet.getTags().get(0).getId());
+//		Assert.assertEquals(js.getInt("tags[1].id"), pet.getTags().get(1).getId());
+//		Assert.assertEquals(js.getInt("tags[2].id"), pet.getTags().get(2).getId());
+		
+		for(int i=0; i<tags.size(); i++) {
+			Assert.assertEquals(js.getInt("tags["+i+"].id"), pet.getTags().get(i).getId());
+		}
+
+		for(int i=0; i<tags.size(); i++) {
+			Assert.assertEquals(js.getString("tags["+i+"].name"), pet.getTags().get(i).getName());
+		}
+
+	
 	
 	
 	@Test
